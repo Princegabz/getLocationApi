12b93672485a506c9295a3bb0dbcb882198e94ae
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-08-28 16:12:36 +0200

    Added color and updated list view

--- app/src/main/java/com/example/getlocationapi/MainActivity.kt
+++ app/src/main/java/com/example/getlocationapi/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.getlocationapi
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLocation = findViewById<Button>(R.id.btnLocation)
+        btnLocation.setBackgroundColor(Color.parseColor("#3FC0C8"))// Change to any color you want
     }
 }
